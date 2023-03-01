115f55036e8cfa23e54ca4917eb1a562ae857ec9
rushi-smb <rushi@smbios.co.in>
2023-03-01 12:55:24 +0530

    Creating Binding (DataView - DataViewModel and Product)

---
+++ CollectionViewDemo/MVVM/Views/DataView.xaml.cs
@@ -0,0 +1,13 @@
+using CollectionViewDemo.MVVM.ViewModels;
+
+namespace CollectionViewDemo.MVVM.Views;
+
+public partial class DataView : ContentPage
+{
+	public DataView()
+	{
+		InitializeComponent();
+
+		BindingContext = new DataViewModel();
+	}
+}
